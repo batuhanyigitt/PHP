73859bd70ca18c3c660c1408af46e4d1f9736ccc
Batuhan Yiğit <70327837+batuhanyigitt@users.noreply.github.com>
2021-09-06 12:20:28 +0300

    Add files via upload

---
+++ phpsecurity2/phpsecurity2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>GÜVENLİK</title>
+</head>
+<body>
+
+<?php
+
+//---- SECURITY ------
+
+/*
+$veri=md5("veri");
+
+setcookie("deneme",$veri,time() + 60*60*24);
+*/
+
+/*
+//md5
+
+//shal
+
+$sifre=123456789;
+
+echo sha1(md5(md5($sifre)));
+*/
+
+
+?>
+
+<?php header("Location: http://localhost/php/index.php");
+
+
+
+
+// KLASÖR GÜVENLİĞİ
+
+
+?>
+
+</body>
+</html>
